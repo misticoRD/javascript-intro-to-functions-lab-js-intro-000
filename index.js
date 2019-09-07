1d14871f81907954d06c5510331978a94f3c758f
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-07 20:07:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function  sayHiToGrandma(string){
 
   if(string.toLowerCase() === lowercase){
     return  "I can't hear you!";
-  }else if(string.toUpperCase() === upercase){
+  }else if(string.toUpperCase() === uppercase){
     return "YES INDEED!";
   }else if(string === "I love you, Grandma."){
     return "I love you, too.";
