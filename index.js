c6c11558d43e865681b6835d8be4d281a4a25123
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-07 19:40:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ function shout(string) {
 let whisper = (string) => string.toLowerCase();
 
 let logShout = (string) => console.log(string.toUpperCase());
+
+let logWhisper = (string) => console.log(string.toLowerCase());
