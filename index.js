7994e120410af447d22b6494e5c3b233e9483a86
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-07 19:47:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ let whisper = (string) => string.toLowerCase();
 let logShout = (string) => console.log(string.toUpperCase());
 
 let logWhisper = (string) => console.log(string.toLowerCase());
+
+function  sayHiToGrandma(string){
+
+  if(string.toLowerCase === lowercase){
+    return  "I can't hear you!";
+  }else if(string.toUpperCase === upercase){
+    return "YES INDEED!";
+  }else if(string === "I love you, Grandma."){
+    return "I love you, too.";
+  }
+}
+
+
